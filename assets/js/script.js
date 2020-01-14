187009562be24a7210839f5b29ecb79ecbc2096a
chriscrichter <chris_richter@outlook.com>
2020-01-14 08:27:38 -0500

    Updated html and js files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,47 +73,47 @@ $(".todo5").val(localStorage.getItem("5:00"));
 var time1 = $(".time1").first().text();
 $(".time1").html( time1 );
 time1 = parseInt(time1[0]);
-var todo9 = document.querySelector("#todo9");
+var todo9 = document.querySelector(".todo9");
 
 var time2 = $(".time2").first().text();
 $(".time2").html( time2 );
 time2 = parseInt(time2[0]);
-var todo10 = document.querySelector("#todo10");
+var todo10 = document.querySelector(".todo10");
 
 var time3 = $(".time3").first().text();
 $(".time3").html( time3 );
 time3 = parseInt(time3[0]);
-var todo11 = document.querySelector("#todo11");
+var todo11 = document.querySelector(".todo11");
 
 var time4 = $(".time4").first().text();
 $(".time4").html( time4 );
 time4 = parseInt(time4[0]);
-var todo12 = document.querySelector("#todo12");
+var todo12 = document.querySelector(".todo12");
 
 var time5 = $(".time5").first().text();
 $(".time5").html( time5 );
 time5 = parseInt(time5[0]);
-var todo1 = document.querySelector("#todo1");
+var todo1 = document.querySelector(".todo1");
 
 var time6 = $(".time6").first().text();
 $(".time6").html( time6 );
 time6 = parseInt(time6[0]);
-var todo2 = document.querySelector("#todo2");
+var todo2 = document.querySelector(".todo2");
 
 var time7 = $(".time7").first().text();
 $(".time7").html( time7 );
 time7 = parseInt(time7[0]);
-var todo3 = document.querySelector("#todo3");
+var todo3 = document.querySelector(".todo3");
 
 var time8 = $(".time8").first().text();
 $(".time8").html( time8 );
 time8 = parseInt(time8[0]);
-var todo4 = document.querySelector("#todo4");
+var todo4 = document.querySelector(".todo4");
 
 var time9 = $(".time9").first().text();
 $(".time9").html( time9 );
 time9 = parseInt(time9[0]);
-var todo5 = document.querySelector("#todo5");
+var todo5 = document.querySelector(".todo5");
 
 
 //function that color codes the various time rows depending on current hour.
