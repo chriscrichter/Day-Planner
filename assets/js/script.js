9ec88a6794ffbc66e7275a1cda9393ac401698bd
chriscrichter <chris_richter@outlook.com>
2020-01-14 17:18:22 -0500

    Updated html and js files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,90 +6,84 @@ $(document).ready(function() {
 var dateParagraph = $("#currentDay");
 var todayDate = moment().format('dddd, MMMM Do YYYY');
 dateParagraph.text(todayDate);
-});
 var currentTime = moment().format('H');
 currentTime = parseInt(currentTime);
 
 //Save buttons will save user inputs to local store and saved inputs will display in the textarea.
+//Getting the hours from the page and converting them into numbers.
+//Also getting each texarea.
 
+var time1 = parseInt(09);
+var todo9 = $('.todo9').get(0);
+$(".todo9").val(localStorage.getItem("9:00"));
 $(".saveTodo9").on("click", function() {
     var description = $(".todo9").val();
     localStorage.setItem("9:00", description);
 });
-$(".todo9").val(localStorage.getItem("9:00"));
 
+var time2 = parseInt(10);
+var todo10 = $(".todo10").get(0);
+$(".todo10").val(localStorage.getItem("10:00"));
 $(".saveTodo10").on("click", function() {
     var description = $(".todo10").val();
     localStorage.setItem("10:00", description);
 });
-$(".todo10").val(localStorage.getItem("10:00"));
 
+var time3 = parseInt(11);
+var todo11 = $(".todo11").get(0);
+$(".todo11").val(localStorage.getItem("11:00"));
 $(".saveTodo11").on("click", function() {
     var description = $(".todo11").val();
     localStorage.setItem("11:00", description);
 });
-$(".todo11").val(localStorage.getItem("11:00"));
 
+var time4 = parseInt(12);
+var todo12 = $(".todo12").get(0);
+$(".todo12").val(localStorage.getItem("12:00"));
 $(".saveTodo12").on("click", function() {
     var description = $(".todo12").val();
     localStorage.setItem("12:00", description);
 });
-$(".todo12").val(localStorage.getItem("12:00"));
 
+var time5 = parseInt(13);
+var todo1 = $(".todo1").get(0);
+$(".todo1").val(localStorage.getItem("1:00"));
 $(".saveTodo1").on("click", function() {
     var description = $(".todo1").val();
     localStorage.setItem("1:00", description);
 });
-$(".todo1").val(localStorage.getItem("1:00"));
 
+var time6 = parseInt(14);
+var todo2 = $(".todo2").get(0);
+$(".todo2").val(localStorage.getItem("2:00"));
 $(".saveTodo2").on("click", function() {
     var description = $(".todo2").val();
     localStorage.setItem("2:00", description);
 });
-$(".todo2").val(localStorage.getItem("2:00"));
 
+var time7 = parseInt(15);
+var todo3 = $(".todo3").get(0);
+$(".todo3").val(localStorage.getItem("3:00"));
 $(".saveTodo3").on("click", function() {
     var description = $(".todo3").val();
     localStorage.setItem("3:00", description);
 });
-$(".todo3").val(localStorage.getItem("3:00"));
 
+var time8 = parseInt(16);
+var todo4 = $(".todo4").get(0);
+$(".todo4").val(localStorage.getItem("4:00"));
 $(".saveTodo4").on("click", function() {
     var description = $(".todo4").val();
     localStorage.setItem("4:00", description);
 });
-$(".todo4").val(localStorage.getItem("4:00"));
 
+var time9 = parseInt(17);
+var todo5 = $(".todo5").get(0);
+$(".todo5").val(localStorage.getItem("5:00"));
 $(".saveTodo5").on("click", function() {
     var description = $(".todo5").val();
     localStorage.setItem("5:00", description);
 });
-$(".todo5").val(localStorage.getItem("5:00"));
-
-
-//Getting the hours from the page and converting them into numbers.
-//Also getting each texarea.
-
-
-var todo9 = document.querySelector(".todo9");
-var todo10 = document.querySelector(".todo10");
-var todo11 = document.querySelector(".todo11");
-var todo12 = document.querySelector(".todo12");
-var todo1 = document.querySelector(".todo1");
-var todo2 = document.querySelector(".todo2");
-var todo3 = document.querySelector(".todo3");
-var todo4 = document.querySelector(".todo4");
-var todo5 = document.querySelector(".todo5");
-
-var time1 = parseInt(09);
-var time2 = parseInt(10);
-var time3 = parseInt(11);
-var time4 = parseInt(12);
-var time5 = parseInt(13);
-var time6 = parseInt(14);
-var time7 = parseInt(15);
-var time8 = parseInt(16);
-var time9 = parseInt(17);
 
 
 //function that color codes the various time rows depending on current hour.
@@ -109,3 +103,5 @@ function changeColor() {
     }
 }
 changeColor();
+
+});
