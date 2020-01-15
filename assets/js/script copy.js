5928810c7d37e1e72c16ac69a8392af6eac6d0c7
chriscrichter <chris_richter@outlook.com>
2020-01-14 23:42:17 -0500

    Updated js file.

--- assets/js/script copy.js
+++ assets/js/script copy.js
@@ -2,14 +2,15 @@
 
 $(document).ready(function() {
 
-// Variables for date and time.
+// Variables for date and time
 var dateParagraph = $("#currentDay");
 var todayDate = moment().format('dddd, MMMM Do YYYY');
 dateParagraph.text(todayDate);
 var currentTime = moment().format('H');
 currentTime = parseInt(currentTime);
 
-//Saving and getting appointments to and from local storage. Todo variables are set up here for the changeColor function below.
+//Save buttons will save appointments to local storage and any that were previously saved will display in the textarea. Time and todo variables are set up here for the changeColor function below.
+var time1 = parseInt(09);
 var todo9 = $('.todo9').get(0);
 $(".todo9").val(localStorage.getItem("9:00"));
 $(".saveTodo9").on("click", function() {
@@ -17,6 +18,7 @@ $(".saveTodo9").on("click", function() {
     localStorage.setItem("9:00", description);
 });
 
+var time2 = parseInt(10);
 var todo10 = $(".todo10").get(0);
 $(".todo10").val(localStorage.getItem("10:00"));
 $(".saveTodo10").on("click", function() {
@@ -24,6 +26,7 @@ $(".saveTodo10").on("click", function() {
     localStorage.setItem("10:00", description);
 });
 
+var time3 = parseInt(11);
 var todo11 = $(".todo11").get(0);
 $(".todo11").val(localStorage.getItem("11:00"));
 $(".saveTodo11").on("click", function() {
@@ -31,6 +34,7 @@ $(".saveTodo11").on("click", function() {
     localStorage.setItem("11:00", description);
 });
 
+var time4 = parseInt(12);
 var todo12 = $(".todo12").get(0);
 $(".todo12").val(localStorage.getItem("12:00"));
 $(".saveTodo12").on("click", function() {
@@ -38,6 +42,7 @@ $(".saveTodo12").on("click", function() {
     localStorage.setItem("12:00", description);
 });
 
+var time5 = parseInt(13);
 var todo1 = $(".todo1").get(0);
 $(".todo1").val(localStorage.getItem("1:00"));
 $(".saveTodo1").on("click", function() {
@@ -45,6 +50,7 @@ $(".saveTodo1").on("click", function() {
     localStorage.setItem("1:00", description);
 });
 
+var time6 = parseInt(14);
 var todo2 = $(".todo2").get(0);
 $(".todo2").val(localStorage.getItem("2:00"));
 $(".saveTodo2").on("click", function() {
@@ -52,6 +58,7 @@ $(".saveTodo2").on("click", function() {
     localStorage.setItem("2:00", description);
 });
 
+var time7 = parseInt(15);
 var todo3 = $(".todo3").get(0);
 $(".todo3").val(localStorage.getItem("3:00"));
 $(".saveTodo3").on("click", function() {
@@ -59,6 +66,7 @@ $(".saveTodo3").on("click", function() {
     localStorage.setItem("3:00", description);
 });
 
+var time8 = parseInt(16);
 var todo4 = $(".todo4").get(0);
 $(".todo4").val(localStorage.getItem("4:00"));
 $(".saveTodo4").on("click", function() {
@@ -66,6 +74,7 @@ $(".saveTodo4").on("click", function() {
     localStorage.setItem("4:00", description);
 });
 
+var time9 = parseInt(17);
 var todo5 = $(".todo5").get(0);
 $(".todo5").val(localStorage.getItem("5:00"));
 $(".saveTodo5").on("click", function() {
@@ -73,8 +82,8 @@ $(".saveTodo5").on("click", function() {
     localStorage.setItem("5:00", description);
 });
 
-//Function for changing appointment color based on the hour
-var hours = [parseInt(09), parseInt(10), parseInt(11), parseInt(12), parseInt(13), parseInt(14), parseInt(15), parseInt(16), parseInt(17)];
+//Function for changing appointment color based on the hour.
+var hours = [time1, time2, time3, time4, time5, time6, time7, time8, time9];
 var inputs = [todo9, todo10, todo11, todo12, todo1, todo2, todo3, todo4, todo5];
 function changeColor() {
     for (i = 0; i < hours.length; i ++) {
